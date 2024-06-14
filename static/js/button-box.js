38b312474427a73356f6f95e941681256fe4b35f
Vincent-Juma <vincentjuma64@gmail.com>
2024-06-14 18:48:56 +0300

    removed contents

--- static/js/button-box.js
+++
@@ -1,6 +0,0 @@
-var elements = document.getElementsByClassName("button-box");
-Array.prototype.slice.call( elements ).forEach(element => {
-    element.addEventListener("click", function(e) {
-        location.href = element.getAttribute("href");
-    }, false);
-});
